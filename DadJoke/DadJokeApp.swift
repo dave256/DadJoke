10be46a705fe9ba07297d1531e61a402a1563d0e
Dave Reed <dave256@mac.com>
2023-01-14 19:14:05 -0500

    Initial Commit

---
+++ DadJoke/DadJokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeApp.swift
+//  DadJoke
+//
+//  Created by David Reed on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
